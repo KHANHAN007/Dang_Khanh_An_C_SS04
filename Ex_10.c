e6de2c8f0e9bc9518d0cbfb76673fb66f09f69b6
KHANHAN007 <khanhanke@gmail.com>
2024-11-24 19:25:11 +0700

    Add files via upload

--- Ex_10.c
+++ Ex_10.c
@@ -6,28 +6,28 @@ int main()
     printf("Nhap vao 3 so nguyen: ");
     scanf("%d%d%d", &firstNum, &secondNum, &thirstNum);
 
-    // int min = (firstNum < secondNum) ? ((firstNum < thirstNum) ? firstNum : thirstNum) : ((secondNum < thirstNum) ? secondNum : thirstNum);
-    // int max = (firstNum > secondNum) ? ((firstNum > thirstNum) ? firstNum : thirstNum) : ((secondNum > thirstNum) ? secondNum : thirstNum);
-    // int mid = (firstNum + secondNum + thirstNum) - (min + max);
-    // printf("%d %d %d", min, mid, max);
-    int tmp = 0;
-    if (firstNum > secondNum)
-    {
-        tmp = firstNum;
-        firstNum = secondNum;
-        secondNum = tmp;
-    }
-    if (firstNum > thirstNum)
-    {
-        tmp = firstNum;
-        firstNum = thirstNum;
-        thirstNum = tmp;
-    }
-    if (secondNum > thirstNum)
-    {
-        tmp = secondNum;
-        secondNum = thirstNum;
-        thirstNum = tmp;
-    }
-    printf("%d %d %d", firstNum, secondNum, thirstNum);
+    int min = (firstNum < secondNum) ? ((firstNum < thirstNum) ? firstNum : thirstNum) : ((secondNum < thirstNum) ? secondNum : thirstNum);
+    int max = (firstNum > secondNum) ? ((firstNum > thirstNum) ? firstNum : thirstNum) : ((secondNum > thirstNum) ? secondNum : thirstNum);
+    int mid = (firstNum + secondNum + thirstNum) - (min + max);
+    printf("%d %d %d", min, mid, max);
+    // int tmp = 0;
+    // if (firstNum > secondNum)
+    // {
+    //     tmp = firstNum;
+    //     firstNum = secondNum;
+    //     secondNum = tmp;
+    // }
+    // if (firstNum > thirstNum)
+    // {
+    //     tmp = firstNum;
+    //     firstNum = thirstNum;
+    //     thirstNum = tmp;
+    // }
+    // if (secondNum > thirstNum)
+    // {
+    //     tmp = secondNum;
+    //     secondNum = thirstNum;
+    //     thirstNum = tmp;
+    // }
+    // printf("%d %d %d", firstNum, secondNum, thirstNum);
 }
